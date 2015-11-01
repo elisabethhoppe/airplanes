b4d9e54685806006add88934a74b777c220a4dbc
Elisabeth Hoppe <Elisabeth@elisabeths-mbp.speedport_w_724v_typ_a_05011603_00_007>
2015-11-01 21:47:33 +0100

    AirplnaePhotoManager und PhotoFactory added

---
+++ src/main/java/org/wahlzeit/model/AirplanePhotoManager.java
@@ -0,0 +1,13 @@
+package org.wahlzeit.model;
+
+/**
+ *  A photo manager provides access to and manages airplane photos.
+ * 
+ * @version 1.0
+ * 
+ * @date 1.11.2015
+ *
+ */
+public class AirplanePhotoManager extends PhotoManager {
+
+}
