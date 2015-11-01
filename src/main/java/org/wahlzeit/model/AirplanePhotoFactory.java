aef1354eb60daa69b20958da2d45a1890db8c107
Elisabeth Hoppe <Elisabeth@elisabeths-mbp.speedport_w_724v_typ_a_05011603_00_007>
2015-11-01 21:56:00 +0100

    minor changes

--- src/main/java/org/wahlzeit/model/AirplanePhotoFactory.java
+++ src/main/java/org/wahlzeit/model/AirplanePhotoFactory.java
@@ -1,5 +1,13 @@
 package org.wahlzeit.model;
 
-public class AirplanePhotoFactory {
+/**
+ * A Factory for creating airplane photos and related objects.
+ * 
+ * @version 1.0
+ * 
+ * @date 1.11.2015
+ *
+ */
+public class AirplanePhotoFactory extends PhotoFactory{
 
 }
